e4a295821d1151e74802112db643ef04ecd0dc54
juacostar <juacostar@unal.edu.co>
2023-03-13 12:22:33 -0500

    creating project

---
+++ backend/credibanco/src/test/java/com/example/credibanco/CredibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.credibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
